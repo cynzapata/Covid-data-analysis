ab1d12be1c6bb6a80cb68d01e97b9b20bd4985e8
cynzapata <97005001+cynzapata@users.noreply.github.com>
2023-12-15 01:39:52 -0600

    first commit

---
+++ covid-analysis.sql
@@ -0,0 +1,13 @@
+Select * 
+From PortfolioProject..CovidDeaths
+order by 3,4
+
+--Select * 
+--From PortfolioProject..CovidVaccinations
+--order by 3,4
+
+--Select data
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From PortfolioProject..CovidDeaths
+order by 1,2
