772d78f8518736a0f1e30628d85d2f60f2247c57
cynzapata <97005001+cynzapata@users.noreply.github.com>
2023-12-15 02:00:51 -0600

    looked at total cases vs total deaths

--- covid-analysis.sql
+++ covid-analysis.sql
@@ -11,3 +11,8 @@ order by 3,4
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
 order by 1,2
+
+--Looking at Total Cases vs Total Deaths
+Select Location, date, total_cases, total_deaths, (CONVERT(float, total_deaths)/ total_cases)*100
+From PortfolioProject..CovidDeaths
+order by 1,2
